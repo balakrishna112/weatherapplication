300b34f9082b980fcab6bf603141fd550486cbdb
balakrishna112 <chandanambalakrishna@gmail.com>
2020-09-05 13:00:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import Forecast from "./components/Forecast/Forecast";
+import Logo from "./components/Logo/Logo";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Logo />
+        <h1>React Weather App</h1>
+      </header>
+      <main>
+        <Forecast />
+      </main>
+      <footer>Page created by Balakrishna</footer>
+    </div>
+  );
+}
+
+export default App;
